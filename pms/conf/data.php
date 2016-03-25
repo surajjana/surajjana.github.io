fc4b5166edfeabc81405ff2cbfa0a2c924547bd0
Suraj Kumar Jana <surajjana2@gmail.com>
2016-03-25 21:35:54 +0530

    hIQ new commit @Suraj

--- pms/conf/data.php
+++ pms/conf/data.php
@@ -2,8 +2,8 @@
 
 $data = $_REQUEST['data'];
 $res = json_decode($data);
-$res = json_decode($res)
-var_dump($res);
-//echo nl2br("Name : ".$res->{'data'}[0]->{'name'}."\nAddress : ".$res->{'data'}[0]->{'address'}."\nCity : ".$res->{'data'}[0]->{'city'}."\nState : ".$res->{'data'}[0]->{'state'});
+$res = json_decode($res);
+
+echo nl2br("Name : ".$res->{'data'}[0]->{'name'}."\nAddress : ".$res->{'data'}[0]->{'address'}."\nCity : ".$res->{'data'}[0]->{'city'}."\nState : ".$res->{'data'}[0]->{'state'});
 
 ?>
